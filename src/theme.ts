9a5656954ba871dabf90ffccc9bc741b91364f9b
Avoler0 <jys1391@gmail.com>
2022-03-03 15:57:34 +0900

    Start Setting

--- src/theme.ts
+++ src/theme.ts
@@ -1,12 +1,14 @@
-import {DefaultTheme} from "styled-components";
+import { DefaultTheme } from "styled-components";
 
-export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "#f5f6fa",
-  accentColor: "#44bd32",
-}
-export const lightTheme: DefaultTheme = {
-  bgColor: "whitesmoke",
-  textColor: "black",
-  accentColor: "#44bd32",
-}
+export const theme: DefaultTheme = {
+  red: "#E51013",
+  black: {
+    veryDark: "#141414",
+    darker: "#181818",
+    lighter: "#2F2F2F",
+  },
+  white: {
+    lighter: "#fff",
+    darker: "#e5e5e5",
+  },
+};
